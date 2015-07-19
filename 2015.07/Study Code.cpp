ff853ef09f081b96858786490a724cb8494a659f
BaeSeonWoong <helena5183@gmail.com>
2015-07-19 16:44:05 +0900

    20150719 과제 업로드

--- 2015.07/Study Code.cpp
+++ 2015.07/Study Code.cpp
@@ -24,24 +24,21 @@ void B(int);
 
 int main(){
 	while (1)
-	A();
-
+		A();
 }
 
 void A(){
 
 	int hour, minutes;
-
 	scanf_s("%d %d", &hour, &minutes);
-	printf("%d %d\n", hour, minutes);
 	B((hour * 60) + minutes);
 }
 
 void B(int time){
-	//printf("%d %d", time / 60, time % 60);
 	int newtime = time - 45;
-	if (newtime / 60 <= 0)
-		newtime +=(60 * 24);
-	printf("%d %d\n", newtime / 60, newtime % 60);
 
+	if (newtime / 60 <= 0)
+		newtime += (60 * 24);
+	printf("%d %d\n", newtime / 60, newtime % 60);
 }
+
